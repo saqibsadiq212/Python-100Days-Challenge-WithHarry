b4657cfa2da224dc379562be336262f2f6a56819
Muhammad Saqib Sadiq <59938272+saqibsadiq212@users.noreply.github.com>
2023-01-03 21:09:01 +0000

    Comments, Separators

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('Hello')
+"""
+This is for testing of Escape Sequence
+This is multi Line Commenting
+"""
+print("My name is \"Muhammad Saqib\"\nI am 26 Years Old.")
+print("I live in Lahore", "Pakistan", sep="-", end=".")
