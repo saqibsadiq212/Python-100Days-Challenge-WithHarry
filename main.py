6ffd353c8bc283f60d316a78b1784f074b8ff7e7
Muhammad Saqib Sadiq <59938272+saqibsadiq212@users.noreply.github.com>
2023-01-09 20:21:40 +0000

    Arithmetic Operators

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 """
-This is for testing of Python Data Types
+This is for testing Python Arithmetic Operators
 """
 
-a = 123
-b = 'Saqib'
-c = True
-d = None
-e = [1,2,3,[4,5],['a','b']]
-f = (1,2,'a',3,'b')
-g = {'name': 'Saqib', 'age':26}
-
-print('Type of {0} is: {1}'.format(a, type(a)))
-print('Type of {0} is: {1}'.format(b, type(b)))
-print('Type of {0} is: {1}'.format(c, type(c)))
-print('Type of {0} is: {1}'.format(d, type(d)))
-print('Type of {0} is: {1}'.format(e, type(e)))
-print('Type of {0} is: {1}'.format(f, type(f)))
-print('Type of {0} is: {1}'.format(g, type(g)))
+print("Addition: ", 5 + 3)
+print("Subtraction: ", 5 - 3)
+print("Multiplication: ", 5 * 3)
+print("Division: ", 5 / 3)
+print("Expotenional: ", 5**3)
+print("Modulus: ", 5 % 3)
+print("Float Division: ", 5 // 3)
