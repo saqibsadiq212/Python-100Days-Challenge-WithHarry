7e1529b06c0d5cc4af5777d1a5dcbd5128f14ae0
Muhammad Saqib Sadiq <59938272+saqibsadiq212@users.noreply.github.com>
2023-01-09 20:09:23 +0000

    Python Data Types

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 """
-This is for testing of Escape Sequence
-This is multi Line Commenting
+This is for testing of Python Data Types
 """
-print("My name is \"Muhammad Saqib\"\nI am 26 Years Old.")
-print("I live in Lahore", "Pakistan", sep="-", end=".")
+
+a = 123
+b = 'Saqib'
+c = True
+d = None
+e = [1,2,3,[4,5],['a','b']]
+f = (1,2,'a',3,'b')
+g = {'name': 'Saqib', 'age':26}
+
+print('Type of {0} is: {1}'.format(a, type(a)))
+print('Type of {0} is: {1}'.format(b, type(b)))
+print('Type of {0} is: {1}'.format(c, type(c)))
+print('Type of {0} is: {1}'.format(d, type(d)))
+print('Type of {0} is: {1}'.format(e, type(e)))
+print('Type of {0} is: {1}'.format(f, type(f)))
+print('Type of {0} is: {1}'.format(g, type(g)))
